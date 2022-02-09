18fb53a0f925a16c549cb113cc96e386df956539
Adrien Guesnel <adrien-guesnel@users.noreply.github.com>
2022-02-09 17:50:54 +0100

    Use express-rate-limit to limit access to post and delete routes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import rateLimit from "express-rate-limit";
 import fs from "fs";
 import helmet from "helmet";
 import Jimp from "jimp";
@@ -16,11 +17,21 @@ const APP_ORIGIN = process.env.APP_ORIGIN || "*";
 const CACHE_DURATION = Number(process.env.CACHE_DURATION) || 300;
 const SAVE_MAX_HEIGHT = Number(process.env.SAVE_MAX_HEIGHT) || 1080;
 const SAVE_MAX_WIDTH = Number(process.env.SAVE_MAX_WIDTH) || 1920;
+const API_WINDOW_MIN_DELAY = Number(process.env.API_WINDOW_MIN_DELAY) || 15;
+const API_LIMIT_REQUESTS_BY_WINDOW_AND_IP =
+  Number(process.env.API_LIMIT_REQUESTS_BY_WINDOW_AND_IP) || 50;
 
 const app = express();
 
 app.use(bodyParser.raw({ type: ["image/*"], limit: "10mb" }));
 app.use(helmet());
+
+const apiLimiter = rateLimit({
+  windowMs: API_WINDOW_MIN_DELAY * 60 * 1000,
+  max: API_LIMIT_REQUESTS_BY_WINDOW_AND_IP,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 const corsOptions = {
   origin: APP_ORIGIN,
@@ -78,10 +89,15 @@ app.get("/img/:file", async function (req, res) {
   });
 });
 
-app.post("/img/:name", function (req, res) {
+app.post("/img/:name", apiLimiter, function (req, res) {
   const filename = req.params.name;
   const imgData = req.body;
   const key = req.query.key;
+
+  if (key !== API_SECRET) {
+    res.status(403).send("You are unauthorized to upload image");
+    return;
+  }
 
   if (!filename) {
     res.status(500).send("No filename found");
@@ -90,11 +106,6 @@ app.post("/img/:name", function (req, res) {
 
   if (!imgData || JSON.stringify(imgData) === "{}") {
     res.status(500).send("No img data found into the request");
-    return;
-  }
-
-  if (key !== API_SECRET) {
-    res.status(500).send("You are unauthorized to upload image");
     return;
   }
 
@@ -118,19 +129,19 @@ app.post("/img/:name", function (req, res) {
     });
 });
 
-app.delete("/img/:filename", function (req, res) {
+app.delete("/img/:filename", apiLimiter, function (req, res) {
   const { key } = req.query;
 
   const filename = req.params.filename;
   const filepath = path.resolve(`public/images/${filename}`);
 
-  if (!fs.existsSync(filepath)) {
-    res.status(404).send("Image not found");
+  if (key !== API_SECRET) {
+    res.status(403).send("You are unauthorized to delete image");
     return;
   }
 
-  if (key !== API_SECRET) {
-    res.status(500).send("You are unauthorized to delete image");
+  if (!fs.existsSync(filepath)) {
+    res.status(404).send("Image not found");
     return;
   }
 
